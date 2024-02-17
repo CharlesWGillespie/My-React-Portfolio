bf5874efdace3e22a1e93a102c80225a4c3d6173
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-17 15:59:16 -0500

    added styling to homepage now will work on about me page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,20 @@
 export default function Home() {
     return(
     <div className="fullstack">
-        <h2>Full-stack Development</h2>
-        <p className="hp-p-tag">Combining the best of both worlds, allowing me to work on both the front-end and back-end aspects of web development. With experience in languages like HTML, CSS, JavaScript, and frameworks like React for front-end development, coupled with back-end technologies such as Node.js, Express, and databases like MongoDB and SQL. </p>
+        <h2>Crafting Visually Stunning Experiences</h2>
+        <p className="hp-p-tag">As a full-stack developer, I specialize in creating captivating user experiences through meticulous CSS styling and layout. From responsive designs to intricate UI components, I excel in enhancing the visual appeal of web applications with pixel-perfect precision. </p>
 
       <h3>End-to-End Solutions</h3>
-      <p className="hp-p-tag"> As a full-stack developer, I excel in crafting end-to-end solutions that encompass everything from designing user interfaces to implementing server-side logic and database management.</p>
+      <p className="hp-p-tag"> I offer comprehensive solutions covering UI design, server-side logic, and database management. Proficient in JavaScript, React, Node.js, Express, MongoDB, and SQL, I seamlessly bridge front-end and back-end development for holistic web solutions.</p>
       <br />
 
       <h3>Continuous Learning and Adaptability</h3>
-      <p className="hp-p-tag">In the dynamic field of full-stack development, continuous learning and adaptability are paramount. I am committed to staying abreast of emerging technologies, trends, and best practices in web development to deliver cutting-edge solutions to my projects.</p>
+      <p className="hp-p-tag">In the ever-evolving field of full-stack development, I prioritize continuous learning and adaptability. I stay updated on emerging technologies and best practices to deliver cutting-edge solutions for every project.</p><br />
+
+      <h3>Elevated by UConn Coding Bootcamp Experience</h3>
+      <p className="hp-p-tag">As student of the UConn Full-stack Coding Bootcamp, I've honed my skills through hands-on projects and collaborative learning. Equipped with strong teamwork and communication abilities, I thrive in dynamic development environments, ready to tackle any challenge head-on.</p>
+    
     </div>
+
     );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,11 @@
 export default function Intro(){
     return(
-    <div className='intro'><h2>Hi There!</h2>
-    <img  className="imageHP" src="public/assets/website-img.png.jpg" alt="Picture of Charles Gillespie at his desk" />
-      <h2>My Name is</h2>
+    <div id="img-trial" className='intro'>
+     <div className="intro-container">
+      <h2>Hi There! My Name is</h2>
       <h1>Charles Gillespie</h1>
       <h4>I am a Junior Full-Stack Developer!</h4>
+      </div>
     </div>
     );
 }
