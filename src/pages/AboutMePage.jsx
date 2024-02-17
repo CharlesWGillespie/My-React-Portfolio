f1b93f37ecb962eb55d1eac1b0c441358e76b20f
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-17 16:11:04 -0500

    added some styling to about me page need to continure

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,10 +1,12 @@
 import AboutMe from '../components/AboutMe'
 import Header from '../components/Intro'
+import Navbar from '../components/NavBar'
 
+import '../css/aboutme.css'
 export default function AboutMePage() {
 return(
     <>
-    
+    <Navbar/>
     <Header />
     <AboutMe />
     </>
