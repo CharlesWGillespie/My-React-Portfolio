249898e779ee900ec6e0eff83c627b197ce2ea7b
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-19 15:32:37 -0500

    fixed spelling err

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(){
         <footer>
             <h2 className="connect-title">Connect With Me!</h2>
             <div className="connect-container">
-                <a className="connect-with-me" href="https://www.linkedin.com/in/charles-gillespie-b22888280/">LinkdeIn</a>
+                <a className="connect-with-me" href="https://www.linkedin.com/in/charles-gillespie-b22888280/">LinkedIn</a>
                 <a className="connect-with-me" href="https://github.com/CharlesWGillespie">GitHub</a>
                 <a className="connect-with-me" href="http://localhost:5173/contact">Email</a>
                 <a className="connect-with-me" href="https://twitter.com/goopcode">X.com</a>
