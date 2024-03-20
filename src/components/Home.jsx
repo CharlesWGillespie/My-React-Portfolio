5c1f8f7b47587e7c6de6e5c42da6cbf2de7e8763
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-03-20 12:58:44 -0400

    updtated front page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,13 @@ export default function Home() {
     <div className="fullstack-container">
       <div className="fullstack">
           <h2>Crafting Visually Stunning Experiences</h2>
-          <p className="hp-p-tag">As a full-stack developer, I specialize in creating captivating user experiences through meticulous CSS styling and layout. From responsive designs to intricate UI components, I excel in enhancing the visual appeal of web applications with pixel-perfect precision. </p>
+          <p className="hp-p-tag">As a full-stack developer, I specialize in creating captivating user experiences through CSS styling and layout. From responsive designs to intricate UI components, I excel in enhancing the visual appeal of web applications with pixel-perfect precision. </p>
 
         <h3>End-to-End Solutions</h3>
-        <p className="hp-p-tag"> I offer comprehensive solutions covering UI design, server-side logic, and database management. Proficient in HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and SQL, I seamlessly bridge front-end and back-end development for holistic web solutions.</p>
+        <p className="hp-p-tag"> I offer comprehensive solutions covering UI design, server-side logic, and database management. Proficient in HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and SQL.</p>
         <br />
-
+        <h3>Strengths</h3>
+        <p className="hp-p-tag">My strengths include front end technologies including HTML, CSS, JavaScript and React.</p>
         <h3>Continuous Learning and Adaptability</h3>
         <p className="hp-p-tag">In the ever-evolving field of full-stack development, I prioritize continuous learning and adaptability. I stay updated on emerging technologies and best practices to deliver cutting-edge solutions for every project.</p><br />
 
